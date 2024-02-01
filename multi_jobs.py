26aee9d61b62505f82031fabfb8b2346dbdcfd89
Jaeseok Lee <jaeseok0708@gmail.com>
2024-02-01 18:15:39 +0900

    multi process, parallel jobs

---
+++ multi_jobs.py
@@ -0,0 +1,72 @@
+import numpy as np
+import numpy.matlib 
+from bloch import bloch_rotate
+import matplotlib.pyplot as plt
+from pulse import shaped_pulse, hard_pulse, import_shaped_pulse
+from joblib import Parallel, delayed
+
+def run_simulation(time_temp):
+    global Gamma, BW
+    Gamma = 42.58  # kHz/mT
+
+    M0 = 1
+    M_equilibrium = np.array([0, 0, M0])
+    BW = 8  # kHz
+    df = np.linspace(-BW/2, BW/2, num=1000)
+    N = 1000
+
+    M = np.tile(M_equilibrium, (len(df), 1)).T
+    M = M.astype(float)
+
+    df_temp = np.ndarray(shape=(3, 1, 1000))
+    RF_temp = np.ndarray(shape=(3, 1, N))
+    t_max_temp = np.ndarray(shape=(3, 1, N))
+    file_path = 'wave/TangAmplitudeMod'
+
+    # shaped Pulse (sine)
+    i = 0
+    print(f'first pulse "{file_path}" running...')
+    M, df_temp[i], RF_temp[i], t_max_temp[i] = import_shaped_pulse(M, np.pi / 2, "x", time_temp / 10, file_path, BW, Gamma)
+
+    # hard Pulse
+    i += 1
+    print("second pulse running...")
+    M, df_temp[i], RF_temp[i], t_max_temp[i] = hard_pulse(M, -np.pi, "x", 0.02, N, BW, Gamma)
+
+    # shaped Pulse (sine)
+    i += 1
+    print(f'third pulse "{file_path}" running...')
+    M, df_temp[i], RF_temp[i], t_max_temp[i] = import_shaped_pulse(M, np.pi / 2, "x", time_temp / 10, file_path, BW, Gamma)
+
+    RF_t = np.append(RF_temp[0, :, :], RF_temp[1, :, :])
+    RF_t = np.append(RF_t, RF_temp[2, :, :])
+
+    t_1 = np.arange(0, N, 1) * t_max_temp[0] / N
+    t_2 = np.arange(0, N, 1) * t_max_temp[1] / N + t_max_temp[0]
+    t_3 = np.arange(0, N, 1) * t_max_temp[2] / N + t_max_temp[0] + t_max_temp[1]
+
+    t = np.append(t_1, t_2)
+    t = np.append(t, t_3)
+
+    for n in range(len(df)):
+        if M[2, n] > 0.9:
+            print(df[n])
+            break
+
+    fig, axs = plt.subplots(2, 1)
+    axs[0].plot(t[0] - np.finfo(np.float64).eps, 0)
+    axs[0].plot(t, RF_t.T)
+    axs[0].plot(t[-1] + np.finfo(np.float64).eps, 0)
+    axs[0].set(xlabel='time (ms)', ylabel='RF (mT)')
+    df = df * 1000
+    plt.subplots_adjust(hspace=0.9)
+    axs[1].plot(df, M[2, :], label="Mz")
+    axs[1].plot(df, M[1, :], label="My")
+    axs[1].plot(df, M[0, :], label="Mx")
+    axs[1].set(xlabel='frequency (Hz)', ylabel='flip')
+    axs[1].legend()
+    time_temp = time_temp * 100
+    plt.savefig('{}us_TangAmplitudeMod.png'.format(time_temp), dpi=600)
+
+# Operate parallel jobs
+Parallel(n_jobs=-1)(delayed(run_simulation)(time_temp) for time_temp in range(3, 51))
