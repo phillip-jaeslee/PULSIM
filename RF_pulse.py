d64706619b77e128d91b96261a1f724137048394
Jaeseok Lee <jaeseok0708@gmail.com>
2024-05-18 21:58:41 +0900

    update for faster calculation

--- RF_pulse.py
+++ RF_pulse.py
@@ -35,6 +35,8 @@ final = N/2 + 1
 t = np.arange(init, final-1, 1) * dt
 
 RF = np.ones((1, int(N)))
+print(RF)
+print(np.shape(RF))
 RF = (flip) * RF/np.sum(RF) / (2*np.pi*Gamma*dt)
 BW = 4 # kHz
 df = np.linspace(-BW/2, BW/2, num=100)
@@ -45,13 +47,15 @@ for n in range(len(t)):
     for f in range(len(df)):
         M[:, f]  = bloch_rotate(M[:, f], dt, [np.real(RF[0, n]), np.imag(RF[0, n]), df[f]/Gamma], "x")
     
+t_ext_neg = [t[0] - 1e-5, 0]
+t_ext_pos = [t[-1] + 1e-5, 0]
+print(t_ext_pos)
 
 fig, axs = plt.subplots(2, 1)
-axs[0].plot(t[0]-np.finfo(np.float64).eps, 0)
+axs[0].plot(t_ext_neg[0], t_ext_neg[1])
 axs[0].plot(t, RF.T)
-axs[0].plot(t[-1]+np.finfo(np.float64).eps, 0)
+axs[0].plot(t_ext_pos[0], t_ext_pos[1])
 axs[0].set(xlabel='time (ms)', ylabel='RF (mT)')
-
 axs[1].plot(df, M[2,:], label="Mz")
 axs[1].plot(df, np.sqrt(M[0,:]**2+M[1,:]**2), label="|Mxy|")
 axs[1].set(xlabel='frequency (kHz)', ylabel='flip')
