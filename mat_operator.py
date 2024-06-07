a31a90ac637f255bb990382148c0dee82be46e6c
phillip-jaeslee <jaeseok0708@gmail.com>
2024-06-07 20:47:16 +0900

    operator tested

--- mat_operator.py
+++ mat_operator.py
@@ -9,19 +9,19 @@ class spin_half:
         return torch.stack([
             torch.stack([torch.zeros(1), torch.ones(1)], dim=0),
             torch.stack([torch.ones(1), torch.zeros(1)], dim=0)
-        ], dim=0, dtype=torch.complex128).to(device) / 2
+        ], dim=0).to(device) / 2
 
     def Iy():
         return torch.stack([
             torch.stack([torch.zeros(1), torch.ones(1)], dim=0),
             torch.stack([-torch.ones(1), torch.zeros(1)], dim=0)
-        ], dim=0, dtype=torch.complex128).to(device) * -1j / 2
+        ], dim=0).to(device) * -1j / 2
 
     def Iz():
         return torch.stack([
             torch.stack([torch.ones(1), torch.zeros(1)], dim=0),
             torch.stack([torch.zeros(1), -torch.ones(1)], dim=0)
-        ], dim=0, dtype=torch.complex128).to(device) / 2
+        ], dim=0).to(device) / 2
 
 
     # Unity operator
@@ -29,32 +29,32 @@ class spin_half:
         return torch.stack([
             torch.stack([torch.ones(1), torch.zeros(1)], dim=0),
             torch.stack([torch.zeros(1), torch.ones(1)], dim=0)
-        ], dim=0, dtype=torch.complex128).to(device) / 2
-    
+        ], dim=0).to(device) / 2
+
     # Shift operators
     def Ip():
         return torch.stack([
             torch.stack([torch.zeros(1), torch.ones(1)], dim=0),
             torch.stack([torch.zeros(1), torch.zeros(1)], dim=0)
-        ], dim=0, dtype=torch.complex128).to(device) / 2
+        ], dim=0).to(device) / 2
 
     def In():
         return torch.stack([
             torch.stack([torch.zeros(1), torch.zeros(1)], dim=0),
             torch.stack([torch.ones(1), torch.zeros(1)], dim=0)
-        ], dim=0, dtype=torch.complex128).to(device) / 2
-    
+        ], dim=0).to(device) / 2
+
     # Projection operators
     def Ia():
-        return torch.add(Iz(), unity(), alpha=1/2)
-    
+        return torch.add(spin_half.Iz(), spin_half.unity(), alpha=1/2)
+
     def Ib():
-        return torch.add(-Iz(), unity(), alpha=1/2)
-    
+        return torch.add(-spin_half.Iz(), spin_half.unity(), alpha=1/2)
+
     # Density matrix
     def thermal_eq(GAMMA, B0, T=293.7):
-        return torch.add(Iz() * 1/2 * boltzmann_factor(GAMMA, B0, T), unity(), alpha=1/2, dtype=torch.complex128).to(device)
-    
+        return torch.add(spin_half.Iz() * 1/2 * boltzmann_factor(GAMMA, B0, T), spin_half.unity(), alpha=1/2, dtype=torch.complex128).to(device)
+
 def boltzmann_factor(GAMMA, B0, T=293.7):
     h_bar = 1.05457182E-34
     kB =  1.380649E-23
