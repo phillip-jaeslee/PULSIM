997540410eb6ebab23fbf4ba8d948955ba274727
phillip-jaeslee <jaeseok0708@gmail.com>
2024-06-11 15:18:06 +0900

    update mat_operator and spin_system

--- print.py
+++ print.py
@@ -1,4 +1,5 @@
 from mat_operator import spin_half
+from spin_system import *
 
 print("Ia",spin_half.Ia())
 print("Ib",spin_half.Ib())
@@ -8,3 +9,5 @@ print("Unity",spin_half.unity())
 print("Ix", spin_half.Ix())
 print("Iy", spin_half.Iy())
 print("Iz", spin_half.Iz())
+
+print("spinsys", spin_system(2))
