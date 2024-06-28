7b246609fa784cb152d61cda48305a0e16546765
Jaeseok Lee <jaeseok0708@gmail.com>
2024-06-28 14:00:34 +0900

    merge rotation operator into the mat_operator.py

--- mat_operator.py
+++ mat_operator.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -48,3 +49,63 @@ def boltzmann_factor(GAMMA, B0, T=293.7):
     h_bar = 1.05457182E-34
     kB =  1.380649E-23
     return h_bar * GAMMA * B0 / (kB * T) 
+    
+class torch_rot:
+    """
+    Rotation operators based on PyTorch library
+    """
+
+    def Rx(theta):
+        return torch.stack([
+            torch.stack([torch.ones_like(theta), torch.zeros_like(theta), torch.zeros_like(theta)], dim=0),
+            torch.stack([torch.zeros_like(theta), torch.cos(theta), torch.sin(theta)], dim=0),
+            torch.stack([torch.zeros_like(theta), -torch.sin(theta), torch.cos(theta)], dim=0)
+        ], dim=0).to(device)
+
+    def Ry(theta):
+        return torch.stack([
+            torch.stack([torch.cos(theta), torch.zeros_like(theta), torch.sin(theta)], dim=0),
+            torch.stack([torch.zeros_like(theta), torch.ones_like(theta), torch.zeros_like(theta)], dim=0),
+            torch.stack([-torch.sin(theta), torch.zeros_like(theta), torch.cos(theta)], dim=0)
+        ], dim=0).to(device)
+
+    def Rz(theta):
+        return torch.stack([
+            torch.stack([torch.cos(theta), torch.sin(theta), torch.zeros_like(theta)], dim=0),
+            torch.stack([-torch.sin(theta), torch.cos(theta), torch.zeros_like(theta)], dim=0),
+            torch.stack([torch.zeros_like(theta), torch.zeros_like(theta), torch.ones_like(theta)], dim=0)
+        ], dim=0).to(device)
+
+    def Rot(theta):
+        return torch.stack([
+            torch.stack([torch.cos(theta), torch.sin(theta)], dim=0),
+            torch.stack([-torch.sin(theta), torch.cos(theta)], dim=0)
+        ], dim=0).to(device)
+
+class cpu_rot:
+    """
+    Rotation operators based on Numpy library
+    """
+
+    def Rx(flip):
+        Rx = np.array ([[1, 0, 0],
+                    [0, np.cos(flip), np.sin(flip)],
+                    [0, -np.sin(flip), np.cos(flip)]])
+        return Rx
+
+    def Ry(flip):
+        Ry = np.array ([[np.cos(flip), 0, np.sin(flip)],
+                    [0, 1, 0],
+                    [-np.sin(flip), 0, np.cos(flip)]])
+        return Ry
+
+    def Rz(flip):
+        Rz = np.array ([[np.cos(flip), np.sin(flip), 0],
+                    [-np.sin(flip), np.cos(flip), 0],
+                    [0, 0, 1]])
+        return Rz
+
+    def Rot(flip):
+        Rot = np.array ([[np.cos(flip), np.sin(flip)],
+                    [-np.sin(flip), np.cos(flip)]])
+        return Rot
