5082d789b8d2ca5b2143888f2fce7f14b0d7e9ad
Jaeseok Lee <jaeseok0708@gmail.com>
2024-06-28 11:24:31 +0900

    solve the function updated problem

--- rotation.py
+++ rotation.py
@@ -34,4 +34,26 @@ def Rot(theta):
         torch.stack([-torch.sin(theta), torch.cos(theta)], dim=0)
     ], dim=0).to(device)
 
+class cpu_pulse:
+    def Rx(flip):
+        Rx = np.array ([[1, 0, 0],
+                    [0, np.cos(flip), np.sin(flip)],
+                    [0, -np.sin(flip), np.cos(flip)]])
+        return Rx
 
+    def Ry(flip):
+        Ry = np.array ([[np.cos(flip), 0, np.sin(flip)],
+                    [0, 1, 0],
+                    [-np.sin(flip), 0, np.cos(flip)]])
+        return Ry
+
+    def Rz(flip):
+        Rz = np.array ([[np.cos(flip), np.sin(flip), 0],
+                    [-np.sin(flip), np.cos(flip), 0],
+                    [0, 0, 1]])
+        return Rz
+
+    def Rot(flip):
+        Rot = np.array ([[np.cos(flip), np.sin(flip)],
+                    [-np.sin(flip), np.cos(flip)]])
+        return Rot
